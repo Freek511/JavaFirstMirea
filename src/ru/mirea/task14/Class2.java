27f808c0fdf2484d4e5c53c640e44c418175ca1b
Freek511 <feed03@uandex.ru>
2022-11-15 23:39:54 +0300

    Refactor 15-17 pracs

---
+++ src/ru/mirea/task14/Class2.java
@@ -0,0 +1,11 @@
+package ru.mirea.task14;
+
+public class Class2 {
+    public static boolean func1(String s){
+        return s.equals("abcdefghijklmnopqrstuv18340");
+    }
+    public static void main(String[] args) {
+        System.out.println(Class2.func1("abcdefghijklmnopqrstuv18340"));
+        System.out.println(Class2.func1("kjdnls"));
+    }
+}
