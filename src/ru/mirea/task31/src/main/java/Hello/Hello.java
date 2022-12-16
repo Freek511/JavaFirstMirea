cb69cdcae976d1bacd5f90dc807b0adb4514c9c2
Matveev Artem <112688022+Freek511@users.noreply.github.com>
2022-12-16 13:56:45 +0300

    Prac31 added

---
+++ src/ru/mirea/task31/src/main/java/Hello/Hello.java
@@ -0,0 +1,13 @@
+package ru.mirea.task31.main.java.Hello;
+
+public class Hello {
+    public int calc(int n) {
+        if (n == 0)
+            throw new ArithmeticException();
+        return n + n/2;
+    }
+
+    public int calculate(int n) {
+        return n + n/4;
+    }
+}
---
+++ src/ru/mirea/task31/src/test/java/Hello/HelloTest.java
@@ -0,0 +1,45 @@
+package ru.mirea.task31.test.java.Hello;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import ru.mirea.task31.main.java.Hello.Hello;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloTest {
+
+    @Test
+    void Test1() {
+        Hello test1 = new Hello();
+        int actual = test1.calc(5);
+        int expected = 7;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void Test2() {
+        Hello test1 = new Hello();
+        int actual = test1.calc(6);
+        int expected = 9;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void Test3() {
+        Hello test1 = new Hello();
+        try {
+            int actual = test1.calc(0);
+            Assert.fail("Expected ArithmeticExceptio");
+        }catch (ArithmeticException e) {
+            Assert.assertNotEquals("", e.getMessage());
+        }
+    }
+
+    @Test
+    void Test4() {
+        Hello test1 = new Hello();
+        int actual = test1.calculate(4);
+        int expected = 5;
+        assertEquals(expected, actual);
+    }
+}
